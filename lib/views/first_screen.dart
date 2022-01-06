c9f483849fac8437ce6262605009ef3b2495fcd9
arsh0611 <arshheer.0611@gmail.com>
2022-01-06 19:48:23 +0530

    Added bottom Navigation Bar

--- lib/views/first_screen.dart
+++ lib/views/first_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:newdemoproject/widgets/NavDrawerWIdget.dart';
 import 'package:newdemoproject/providers/counterProvider.dart';
 import 'package:provider/src/provider.dart';
+import 'package:newdemoproject/widgets/customBottomAppBar.dart';
 
 class FirstScreen extends StatefulWidget {
   @override
@@ -10,6 +11,14 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+  String _lastSelected = 'TAB: 0';
+
+  void _selectedTab(int index) {
+    setState(() {
+      _lastSelected = 'TAB: $index';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,22 +44,42 @@ class _FirstScreenState extends State<FirstScreen> {
                 child: Text("Collaboration Chapter2"),
               ),
             ),
+            Text('$_lastSelected'),
         ]
 
         ),
 
       ),
-      floatingActionButton: FloatingActionButton(
-        key: const Key('increment_floatingActionButton'),
-
-        /// Calls `context.read` instead of `context.watch` so that it does not rebuild
-        /// when [Counter] changes.
-        onPressed: () => context.read<Counter>().increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: FABBottomAppBar(
+        centerItemText: 'SELL',
+        color: Colors.grey,
+        selectedColor: Colors.red,
+        notchedShape: CircularNotchedRectangle(),
+        onTabSelected: _selectedTab,
+        items: [
+          FABBottomAppBarItem(iconData: Icons.home, text: 'HOME'),
+          FABBottomAppBarItem(iconData: Icons.chat, text: 'Chat'),
+          FABBottomAppBarItem(iconData: Icons.toys, text: 'MY ADS'),
+          FABBottomAppBarItem(iconData: Icons.account_box, text: 'ACCOUNT'),
+        ],
       ),
+        floatingActionButton: FloatingActionButton(
+          key: const Key('increment_floatingActionButton'),
+
+          /// Calls `context.read` instead of `context.watch` so that it does not rebuild
+          /// when [Counter] changes.
+          onPressed: () => context.read<Counter>().increment(),
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+
+
 
     );
 
   }
 }
+
+
